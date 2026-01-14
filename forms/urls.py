56f98e7dfd25f3b91c1227233d7d53b6d777da9f
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-14 20:12:51 +0200

    Add urls

--- forms/urls.py
+++ forms/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from forms import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("apps.products.urls", namespace="products")),
+    path("", include("apps.users.urls", namespace="users")),
 ]
 
 if not settings.TESTING:
