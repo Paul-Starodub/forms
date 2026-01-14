01f5221d2e448e102d23dea96ac786213b099129
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-14 21:02:11 +0200

    Add forms, urls, and views

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,13 @@
-from django.urls import include
+from django.urls import path
+from apps.products.views import TagCreateView, TagListView, TagDetailView, TagUpdateView, TagDeleteView
 
 app_name = "products"
 
 
-urlpatterns = []
+urlpatterns = [
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="task-type-delete"),
+]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from apps.products.models import Tag
 
-# Create your views here.
+
+class TagCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("products:tag-list")
+
+
+class TagDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Tag
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Tag
+    paginate_by = 20
+
+
+class TagUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("products:tag-list")
+
+
+class TagDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("products:tag-list")
