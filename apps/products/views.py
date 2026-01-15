c7d6f1a906cd9d611658d4f06ed2102f6bfd6fae
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-01-15 08:38:50 +0200

    Add tag templates, forms, and updated views and URLs

--- apps/products/views.py
+++ apps/products/views.py
@@ -36,34 +36,34 @@ from apps.products.models import Tag
 #     return render(request, "products/tag_form.html", context={"form": form})
 
 
-# def create_tag(request):
-#     if request.method == "POST":
-#         form = TagForm(request.POST)
-#
-#         # print(form.errors)
-#         # print(form.is_valid())
-#         # print(form.cleaned_data)
-#         # print(form.as_table())
-#         # print(form.base_fields["name"].label)
-#         # print(form.fields["name"].label)
-#         # print(form.get_context())
-#         # print("data", form.data)
-#         # form.is_valid()
-#         # print("cleaned_data", form.cleaned_data)
-#         # print(form.template_name)
-#         # print("instance", form.instance)
-#         # print("initial", form.initial)
-#
-#         # print(dir(form))
-#
-#
-#
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponse("Tag was created successfully")
-#             # return HttpResponseRedirect(reverse("products:tag-list"))
-#     form = TagForm()
-#     return render(request, "products/tag_form.html", context={"form": form})
+def create_tag(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+
+        # print(form.errors)
+        # print(form.is_valid())
+        # print(form.cleaned_data)
+        # print(form.as_table())
+        # print(form.base_fields["name"].label)
+        # print(form.fields["name"].label)
+        # print(form.get_context())
+        # print("data", form.data)
+        # form.is_valid()
+        # print("cleaned_data", form.cleaned_data)
+        # print(form.template_name)
+        # print("instance", form.instance)
+        # print("initial", form.initial)
+
+        # print(dir(form))
+
+
+
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Tag was created successfully")
+            # return HttpResponseRedirect(reverse("products:tag-list"))
+    form = TagForm()
+    return render(request, "products/tag_form.html", context={"form": form})
 
 
 class TagCreateView(LoginRequiredMixin, generic.CreateView):
